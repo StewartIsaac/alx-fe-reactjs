313adbec64df6957d28835e29fc5c1a90d7eb561
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-12 01:06:45 +0100

    create project 10

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+      <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+      <div>
+        <WelcomeMessage />
+      </div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <MainContent />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
