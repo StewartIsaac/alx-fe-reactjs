b75b523e22556590a7e1c4cef40f4b03d78e5fbf
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-13 11:42:49 +0100

    remove unused props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import React from "react";
 import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
 
@@ -12,28 +9,9 @@ function App() {
     email: "jane.doe@example.com",
   };
   return (
-    <>
-      <div>
-        <UserContext.Provider value={userData}>
-          <ProfilePage />
-        </UserContext.Provider>
-      </div>
-      <div>
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      </div>
-      <div>
-        <WelcomeMessage />
-      </div>
-      <div>
-        <Header />
-      </div>
-      <div>
-        <MainContent />
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
