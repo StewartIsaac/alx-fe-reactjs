7df22a7687fa9fa90801864d2d1fe3d53e76b0d5
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-16 21:09:33 +0100

    refactor props drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import './App.css';
+import React from 'react';
+import UserProfile from './components/UserProfile';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
