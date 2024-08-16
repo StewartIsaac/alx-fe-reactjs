d5d64fb6cbf2f775faaf9b9b101fabdc34bda39b
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-17 00:33:27 +0100

    import ProfilePage

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
-import UserProfile from './components/UserProfile';
 import UserContext from './components/UserContext';
+import ProfilePage from './components/ProfilePage'
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <UserProfile />
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
