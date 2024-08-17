3df2c41e6fef45566c138b21a05ff48866bf5d73
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-17 02:27:46 +0100

    create basic page components

---
+++ my-company/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <Services />
+      <About />
+      <Contact />
+    </>
+  )
+}
+
+export default App
