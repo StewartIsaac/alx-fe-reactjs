ae5632414f77b2865bb7dcdbf140fb1615032eb8
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-17 03:14:23 +0100

    update: App

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-      <Home />
-      <Services />
-      <About />
-      <Contact />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App
