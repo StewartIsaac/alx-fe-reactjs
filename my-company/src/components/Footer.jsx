d979ed65a22dd4e7d12ce96e979b5fa8bb7b4c43
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-17 03:13:48 +0100

    add Footer

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#f0f0f0', textAlign: 'center' }}>
+        <p>&copy; 2023 My Company All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
