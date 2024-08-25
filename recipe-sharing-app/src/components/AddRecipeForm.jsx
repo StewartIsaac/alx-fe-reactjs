79059ea8bf89a8257dd74b6092a225f16afde1f0
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-25 22:16:14 +0100

    recipe app

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const AddRecipeForm = () => {
+    const addRecipe = useRecipeStore(state => state.addRecipe);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addRecipe({ id: Date.now(), title, description });
+        setTitle('');
+        setDescription('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='flex flex-col'>
+            <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+                className='my-0.5 rounded-md p-2'
+            />
+            <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Description"
+                className='my-2 rounded-md p-2 columns-lg'
+            />
+            <button type="submit">Add Recipe</button>
+        </form>
+    );
+};
+
+export default AddRecipeForm;
