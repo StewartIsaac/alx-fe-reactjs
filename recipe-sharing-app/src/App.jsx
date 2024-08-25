0cbf97628495caedcb2933d8874aa79da166534f
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-25 23:59:52 +0100

    update: routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import RecommendationsList from './components/RecommendationsList';
+import FavoritesList from './components/FavoritesList';
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path='/add' element={<AddRecipeForm />} />
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipes" element={<RecipeDetails />} />
+          <Route path="/recommended-recipes" element={<RecommendationsList />} />
+          <Route path="/myfavorite-recipes" element={<FavoritesList />} />
         </Routes>
       </Router>
     </>
