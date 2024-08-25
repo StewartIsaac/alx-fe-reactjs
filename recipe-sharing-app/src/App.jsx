8f56837df9e43125f745fd1a182f6a8dda91d5ed
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-25 23:14:33 +0100

    integrate routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <RecipeList />
-      <AddRecipeForm />
+      {/* <AddRecipeForm /> */}
+      <Router>
+        <Routes>
+          <Route path='/add-recipe' element={<AddRecipeForm />} />
+          <Route path="/home" element={<RecipeList />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,25 @@
+import { useRecipeStore } from './recipeStore';
+import { Link, useParams } from 'react-router-dom';
+
+const RecipeDetails = () => {
+    const { recipeId } = useParams();
+    const recipe = useRecipeStore(state =>
+        state.recipes.find(recipe => recipe.id === recipeId)
+    );
+
+    if (!recipe) {
+        return <div>Recipe not found</div>;
+    }
+
+    return (
+        <div>
+            <h1>{recipe.title}</h1>
+            <p>{recipe.description}</p>
+            <EditRecipeForm recipe={recipe} />
+            <DeleteRecipeButton recipeId={recipeId} />
+            <Link to="/">Back to List</Link>
+        </div>
+    );
+};
+
+export default RecipeDetails;
