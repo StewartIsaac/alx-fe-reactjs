22076d2e4575267b3e0cc7b911c1a7b850b4c649
Gabriel Isaac <stewartisaac90@gmail.com>
2024-09-12 22:33:51 +0100

    update recipe details ui

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -18,7 +18,7 @@ export default function RecipeDetail() {
     return (
         <div className="container mx-auto p-8 ">
             <h1 className="text-4xl font-bold my-8">{recipe.title}</h1>
-            <img src={recipe.image} alt={recipe.title} className="w-full h-80 object-cover rounded-lg mb-4" />
+            <img src={recipe.image} alt={recipe.title} className="w-full h-80 object-cover rounded-lg mb-4 shadow-md" />
             <p className="text-gray-400 mb-4">{recipe.summary}</p>
             <h2 className="text-2xl font-semibold mb-2">Ingredients</h2>
             <ul className="list-disc list-inside mb-4">
