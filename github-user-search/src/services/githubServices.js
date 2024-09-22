03ac74dc8a7585524b1a3a0b00a8869d3f2f1a5e
Gabriel <stewarti3@yahoo.com>
2024-09-22 23:50:15 +0100

    create components

---
+++ github-user-search/src/services/githubServices.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const GITHUB_API_URL = "https://api.github.com/users";
+const GITHUB_SEARCH_API_URL = "https://api.github.com/search/users?q=";
+
+export const fetchUserData = (username) => {
+  return axios.get(`${GITHUB_API_URL}/${username}`);
+};
+
+export const fetchAdvancedUserData = (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} in:login`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>${minRepos}`;
+
+  return axios.get(`${GITHUB_SEARCH_API_URL}${query}`);
+};
